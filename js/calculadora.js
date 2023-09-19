c25fe5664939100a38e42bab6975cc0b0d9e192d
Lucas <lucasgori123@gmail.com>
2023-09-18 23:40:52 -0300

    versión 4.0

--- js/calculadora.js
+++ js/calculadora.js
@@ -23,44 +23,129 @@ class Calculadora {
     potenciar(num, exp) {
         return Math.pow(num, exp);
     }
-}
 
-// DOM
-const calculadora = new Calculadora();
-
-// Obtén una referencia al botón "=" por su id
-const igualBoton = document.getElementById("igualBoton");
-
-const calcularButton = document.getElementById("CalcularButton");
-
-
-// Agrega un evento de clic a ambos botones para realizar la operación
-calcularButton.addEventListener("click", realizarOperacion);
-igualBoton.addEventListener("click", realizarOperacion);
-
-// Función para realizar la operación seleccionada
-function realizarOperacion() {
-    const operacion = document.getElementById("operacion").value;
-    let resultado;
-
-    if (operacion === "raizCuadrada") {
-        resultado = calculadora.raizCuadrada(
-            document.getElementById("numero1").value
-        );
-    } else if (operacion === "raizCubica") {
-        resultado = calculadora.raizCubica(
-            document.getElementById("numero1").value
-        );
-    } else {
-        const numero1 = parseFloat(document.getElementById("numero1").value);
-        const numero2 = parseFloat(document.getElementById("numero2").value);
-
-        if (!isNaN(numero1) && !isNaN(numero2)) {
-            resultado = calculadora[operacion](numero1, numero2);
-        } else {
-            resultado = "Error: Los valores ingresados no son números";
-        }
+    raizCuadrada(num) {
+        return Math.sqrt(parseFloat(num));
     }
 
-    document.getElementById("resultado").textContent = `Resultado: ${resultado}`;
+    raizCubica(num) {
+        return Math.cbrt(parseFloat(num));
+    }
 }
+
+// Crear una instancia de la clase Calculadora
+const calculadora = new Calculadora();
+
+// Obtén una referencia al botón por su id
+const calcularButton = document.getElementById("calcularButton");
+
+// Agrega un evento de clic al botón para realizar la operación
+calcularButton.addEventListener("click", realizarOperacion);
+
+// Función para cambiar la operación seleccionada
+function cambiarOperacion(simbolo) {
+    // Obtener el select de operación
+    const operacionSelect = document.getElementById("operacion");
+
+    // Cambiar la opción seleccionada en el select
+    switch (simbolo) {
+        case '+':
+            operacionSelect.value = 'sumar';
+            break;
+        case '-':
+            operacionSelect.value = 'restar';
+            break;
+        case '*':
+            operacionSelect.value = 'multiplicar';
+            break;
+        case '/':
+            operacionSelect.value = 'dividir';
+            break;
+        // Agrega más casos para otros símbolos si es necesario
+    }
+}
+
+//funciones
+// Función para realizar la operación seleccionada
+function realizarOperacion() {
+    const numero1 = parseFloat(document.getElementById("numero1").value);
+    const numero2 = parseFloat(document.getElementById("numero2").value);
+    const operacion = document.getElementById("operacion").value;
+
+    // Verificar si los números son válidos
+    if (isNaN(numero1) || isNaN(numero2)) {
+        // Mostrar un mensaje de error utilizando SweetAlert2
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor, ingrese números válidos.'
+        });
+        return;
+    }
+
+    try {
+        let resultado;
+        if (operacion === "raizCuadrada" || operacion === "raizCubica") {
+            resultado = calculadora[operacion](numero1);
+        } else {
+            resultado = calculadora[operacion](numero1, numero2);
+        }
+        
+        // Mostrar el resultado utilizando SweetAlert2
+        Swal.fire({
+            icon: 'success',
+            title: 'Resultado',
+            text: `Resultado: ${resultado}`
+        });
+    } catch (error) {
+        // Mostrar un mensaje de error utilizando SweetAlert2
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: `Error: ${error.message}`
+        });
+    }
+}
+// Función para borrar los valores en los campos de entrada y la pantalla de resultado con confirmación
+function borrarPantalla() {
+    // Verificar si los campos tienen valores antes de mostrar el mensaje de confirmación
+    if (
+        document.getElementById("numero1").value !== "" ||
+        document.getElementById("numero2").value !== ""
+    ) {
+        // Mostrar un mensaje de confirmación utilizando SweetAlert2
+        Swal.fire({
+            title: '¿Desea guardar los cambios?',
+            showConfirmButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Guardar',
+            cancelButtonText: 'No guardar',
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+        }).then((result) => {
+            if (result.isConfirmed) {
+              // No hagas nada
+            } else {
+            borrarCampos();
+            Swal.fire('Números borrados', '', 'error');
+            }
+        });
+        
+    }
+}
+
+// Función para borrar los valores en los campos de entrada y la pantalla de resultado
+function borrarCampos() {
+    const numero1Input = document.getElementById("numero1");
+    const numero2Input = document.getElementById("numero2");
+    const resultadoElement = document.getElementById("resultado");
+
+    numero1Input.value = "";
+    numero2Input.value = "";
+    resultadoElement.textContent = "";
+}
+
+
+
+
+
